9da71a3d98ba7f7cfd7c3aa5ac744dd06fbe8167
AlexanderKononov <kononov.nov@mail.ru>
2017-12-21 19:45:12 +0200

    move DP to seq command

--- Cover_emulator.py
+++ Cover_emulator.py
@@ -7,40 +7,41 @@ import random
 # This script run with command '-normal' by 3 arguments:
 # first is command '-normal',
 # second is length of DNA data
-# and third is mean resd depth of normal sample.
+
 # Then script creates template for data with 3 chromosomes and with noted summery DNA length and mean read depth.
 
-def createNormalSample(simulation, DNAlengh, meanDP):
-    project_file = open(simulation, 'w')
-    project_file.write('# ' + simulation + '\n# chr\tstart\tend\tmeanDP\n')
+def createNormalSample(simulation, DNAlengh):
+    simulation_file = open(simulation, 'w')
+    simulation_file.write('# ' + simulation + '\n# chr\tstart\tend\tmeanDP\n')
     for i in range(1, 4):
-        project_file.write(str(i) + '\t' + str(1) + '\t' + str(DNAlengh) + '\t' + str(meanDP) + '\n')
-    project_file.close()
+        simulation_file.write(str(i) + '\t' + str(1) + '\t' + str(DNAlengh) + '\t2\n')
+    simulation_file.close()
 
 
 def test_createNormalSample():
-    createNormalSample('test_createNormalSample', 20, 10)
+    createNormalSample('test_createNormalSample', 20)
     test_file = open('test_createNormalSample', 'r')
     for i in test_file:
         if i[0] != '#':
             result = i.strip().split()
     assert result[1] == '1'
     assert result[2] == '20'
-    assert result[3] == '10'
+    assert result[3] == '2'
 
-# This script run with command '-seq' by 2 arguments:
+# This script run with command '-seq' by 3 arguments:
 # first is command '-seq',
-# second is level of noise.
+# second is mean read depth of normal sample.
+# and third is level of noise.
 # Then script creates data file by template created before and.
 
-def sequenceSample(simulation, noise):
+def sequenceSample(simulation, meanDP, noise):
     with open(simulation, 'r') as input, open(simulation + '.data.txt', 'w') as output:
         for line in input:
             if line[0] == '#':
                 continue
             position_data = line.strip().split()
             for position in range(int(position_data[1]) + 1, int(position_data[2]) + 1):
-                position_DP = int(random.normalvariate(int(position_data[3]), int(noise)))
+                position_DP = int(random.normalvariate(int(position_data[3])*int(meanDP), int(noise)))
                 output.write(position_data[0] + '\t' + str(position) + '\t' + str(position_DP) + '\n')
 
 
